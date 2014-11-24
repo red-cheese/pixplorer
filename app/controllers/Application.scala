e87bd774254d3f0a07e3e457a5a167f3b34adf5d
Alexandra Mikhaylova <mikhaylova.alexandra.a@gmail.com>
2014-11-24 20:43:26 +0300

    Working on frontend

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -17,12 +17,14 @@ import play.api.data.Forms._
 import play.api.mvc._
 import scala.collection.concurrent._
 import scala.collection.mutable._
-import scala.concurrent.{blocking, Future, Await}
+import scala.concurrent.{blocking, future, Future, Await}
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Success, Failure}
 
 case class URL(url: String)
+
+case class Image(src: String, pages: List[String])
 
 object Application extends Controller {
 
@@ -32,40 +34,33 @@ object Application extends Controller {
     )(URL.apply)(URL.unapply)
   )
 
+  /* Helper vals to pixplore */
   val browser = new Browser
+  val protocols = Array("http://", "https://", "ftp://")
 
   def index = Action {
     Ok(views.html.index(urlForm))
   }
-/*
-    response onComplete { // Promises are everywhere!
-      case Success(content) => {
 
-	val matchElements: Elements = Jsoup.parse(content) >> elements("h3.r a")
-	val matches: java.util.Iterator[Element] = matchElements.iterator()
-	while (matches.hasNext()) {
-	  val href = matches.next().attr("href")
-	  mmap.get(imgUrl).get += href -> href
-	}
-
-	imgNum -= 1
-	renderResultsIfReady
-      }
-      case Failure(t) => {
-        println("An error has occurred: " + t.getMessage)
-	imgNum -= 1
-	renderResultsIfReady
-      }
-    }
-  }
-*/
-  def reverseSearchGoogle(imgUrl: String) : Future[String] = {
+  def reverseSearchGoogle(imgUrl: String) : Future[List[String]] = {
     /* Ask Google reverse image search and return future of results */
     val userAgent: String = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.120 Chrome/37.0.2062.120 Safari/537.36"
     val gUrl = url("http://www.google.com/searchbyimage?image_url=" + imgUrl) <:< Map("User-Agent" -> userAgent)
-    val resFuture = Http.configure(_ setFollowRedirects true)(gUrl OK as.String)
+
+    val resFuture: Future[List[String]] = future {
+      val gFuture: Future[String] = Http.configure(_ setFollowRedirects true)(gUrl OK as.String)
+      val gResult = Await.result(gFuture, 60 seconds) // @TODO: Add exception handler, or avoid blocking
+      val result: ListBuffer[String] = new ListBuffer[String]
+      val matchElements: Elements = Jsoup.parse(gResult) >> elements("h3.r a")
+      val matches: java.util.Iterator[Element] = matchElements.iterator()
+      while (matches.hasNext()) {
+	val href = matches.next().attr("href")
+	result += href
+      }
+      result.toList
+    }
     /*
-    /* This is only for debugging purposes */
+    /* This is for debugging purposes only */
     resFuture onComplete {
       case Success(content) => {
 	println("Future resolved")
@@ -91,25 +86,21 @@ object Application extends Controller {
     while (images.hasNext()) {
       val src = images.next().attr("src")
       // @TODO: Handle local files: append http://domain
-      val protocols = Array("http://", "https://", "ftp://") // @TODO: Move above
       if (protocols.foldLeft(false) { (x, y) => x || src.startsWith(y) } ) {
 	imgSrcs += src
       }
     }
 
+    val imgSrcList: List[String] = imgSrcs.toList
+
     /* Multitasking with implicit thread pool */
-    val resultFutures: List[Future[String]] = imgSrcs.toList.map {
+    val resultFutures: List[Future[List[String]]] = imgSrcList.map {
       imgSrc => reverseSearchGoogle(imgSrc)
     }
-    val futureResult: Future[List[String]] = Future.sequence(resultFutures)
-    val result = Await.result(futureResult, 90 seconds) // @TODO: Manage blocking more wisely, maybe add exception handler
-
-
-    Ok(result mkString "=====")
-  }
-
-  def results(url: String) = Action {
-    Ok(views.html.results(url))
+    val futureResult: Future[List[List[String]]] = Future.sequence(resultFutures)
+    val result: List[List[String]] = Await.result(futureResult, 90 seconds) // @TODO: Manage blocking more wisely, maybe add exception handler
+    var merge: List[Image] = (imgSrcList zip result).map { case (i, r) => new Image(i, r) }
+    Ok(views.html.results(merge))
   }
 
 }
